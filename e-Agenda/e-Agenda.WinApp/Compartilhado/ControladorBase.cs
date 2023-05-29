6610a700757d697d7d103c2643e7e15cb5ec430a
FabianSilv <127145889+FabianSilv@users.noreply.github.com>
2023-05-28 22:31:32 -0300

    Add files via upload

---
+++ e-Agenda/e-Agenda.WinApp/Compartilhado/ControladorBase.cs
@@ -0,0 +1,20 @@
+﻿using e_Agenda.WinApp.Compartilhado.Interfaces;
+
+namespace e_Agenda.WinApp.Compartilhado
+{
+    public abstract class ControladorBase : IControladorBase
+    {
+        public abstract string ToolTipInserir { get; }
+        public abstract string ToolTipEditar { get; }
+        public abstract string ToolTipExcluir { get; }
+        public virtual string ToolTipFiltrar { get; }
+
+        public virtual void Filtrar() { }
+        public abstract void Inserir();
+        public abstract void Editar();
+        public abstract void Excluir();
+        public abstract UserControl ObterListagem();
+        public abstract string ObterTipoCadastro();
+    }
+
+}
---
+++ e-Agenda/e-Agenda.WinApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,10 @@
+﻿namespace e_Agenda.WinApp.Compartilhado
+{
+    public abstract class EntidadeBase<IEntidadeBase>
+    {
+        public int id;
+        public abstract void AtualizarInformacoes(IEntidadeBase registroAtualizado);
+        public abstract List<string> Validar();
+
+    }
+}
